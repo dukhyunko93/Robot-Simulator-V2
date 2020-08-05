e34af37276d7753f5c6fa71ee53346be26195ebe
dukhyunko93 <dukhyunko93@gmail.com>
2020-08-05 10:37:16 -0400

    Finished

--- index.js
+++ index.js
@@ -2,5 +2,27 @@ document.addEventListener("DOMContentLoaded", function(){
   createGrid()
   renderBot(currentPosition)
 
-  // ADD CODE HERE!
+  const container = document.querySelector("#moves-container")  
+  
+  document.addEventListener("keydown", function(e){
+    if (e.key === "Delete") {
+      container.lastChild.remove();
+    } else {
+    let direction = e.key.slice(5).toLocaleLowerCase();
+    let list = document.createElement("li");
+    list.innerText = direction;
+    container.append(list);
+    };
+  });
+
+
+  const button = document.querySelector("#move-button")
+
+  button.addEventListener("click", function(){
+  const li = document.querySelector("li");
+  let movement = li.innerText;
+  move(movement);
+  li.remove();
+  });
+
 })
